c447c2763db202773ccf992ce96460d13c370192
kiordev <kiordev@gmail.com>
2024-09-04 17:03:45 +0300

    minor changes

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import flet as ft
 def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.title = "Default Name"
+    page.title = "Background"
     page.bgcolor = "#94B5C1"
-    page.add()
+
+    page.add(main)
 
 ft.app(target=main)
